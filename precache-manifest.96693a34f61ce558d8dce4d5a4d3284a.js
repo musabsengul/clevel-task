e574af8ff5051612bad68f4a63c7b41d0242986b
Musab Sengul <musabsl@outlook.com.tr>
2020-08-30 23:42:08 +0300

    first commit

---
+++ precache-manifest.96693a34f61ce558d8dce4d5a4d3284a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecadf47c28c9df1b7cc71e3f7e213fbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f1662d807f21c36b41c",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1d8d99810b505439c7f2",
+    "url": "/static/css/main.66528226.chunk.css"
+  },
+  {
+    "revision": "5f1662d807f21c36b41c",
+    "url": "/static/js/2.10e4234d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.10e4234d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8d99810b505439c7f2",
+    "url": "/static/js/main.aab712fd.chunk.js"
+  },
+  {
+    "revision": "8fb414bc39d2ae9289f4",
+    "url": "/static/js/runtime-main.082f430a.js"
+  }
+]);
